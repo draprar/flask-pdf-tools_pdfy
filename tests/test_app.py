5b8ab9ee1d9d948c96532f9bca91777eb1c5e4f2
Walery <waleron@gmail.com>
2024-12-31 16:34:00 +0100

    Finishing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,9 @@
-import os
-import pytest
 import flask_app
+import pytest
 from io import BytesIO
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture
